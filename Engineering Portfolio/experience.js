8d0afc771b66fa753b8cae99e2df6e2e9220a750
benrschroeder <69219560+benrschroeder@users.noreply.github.com>
2022-03-16 00:58:45 -0400

    Add files via upload

--- Engineering Portfolio/experience.js
+++ Engineering Portfolio/experience.js
@@ -24,3 +24,18 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('#menu')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
